30928bf85f803740ef0990964235855bc70f8e1c
Vinicius Ribeiro <vinicius.ribeiro@adpmnet.com.br>
2021-09-21 17:38:46 -0300

    Aula 72 - Criando variaveis de ambiente

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { LogoProps } from 'types/api'
+import { getImageUrl } from 'utils/getImageUrl'
 import * as S from './styles'
 
-const Logo = ({ alternativeText, url }: LogoProps) => <S.LogoWrapper src={`http://localhost:1337${url}`} alt={alternativeText} />
+const Logo = ({ alternativeText, url }: LogoProps) => <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 
 export default Logo
